b84c769dd2bc40f4993785c0bad4aee8638c24b8
topser9 <topser181@gmail.com>
2021-02-23 06:13:13 +0700

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,123 @@
+#
+# Copyright (C) 2020 topser9
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/a20
+
+# OTA Assert
+TARGET_OTA_ASSERT_DEVICE := a20,a20dd
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a73
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_USES_64_BIT_BINDER := true
+
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+# Build fails without this
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := exynos7884B
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive androidboot.hardware=exynos7884B
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
+
+# Recovery DTBO
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/recovery_dtbo
+
+# mkbootimg arguments
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset 0x01000000
+BOARD_MKBOOTIMG_ARGS += --tags_offset 0x00000100
+BOARD_MKBOOTIMG_ARGS += --header_version 1
+BOARD_MKBOOTIMG_ARGS += --board SRPSA16A009RU
+
+# Platform
+BOARD_VENDOR := samsung
+TARGET_SOC := universal7885
+TARGET_BOARD_PLATFORM := universal7884B
+TARGET_BOARD_PLATFORM_GPU := Mali-G71 MP2
+
+# Filesystem
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# System as root
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+AB_OTA_UPDATER := false
+
+# Workaround for error copying vendor files to recovery ramdisk
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
+
+# Do not go full treble for recovery
+PRODUCT_FULL_TREBLE_OVERRIDE := false
+
+# Add Timezone database
+PRODUCT_COPY_FILES += \
+	system/timezone/output_data/iana/tzdata:$(TARGET_ROOT_OUT)/system/usr/share/zoneinfo/tzdata
+
+# VNDK
+BOARD_VNDK_VERSION := current
+
+# TWRP specific build flags
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+# Do not set up legacy properties
+TW_NO_LEGACY_PROPS := true
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 96
+TW_USE_TOOLBOX := true
+TW_Y_OFFSET := 80
+TW_H_OFFSET := -80
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_INCLUDE_NTFS_3G := true
+TW_EXCLUDE_TWRPAPP := true
+TW_EXTRA_LANGUAGES := true
+TW_USE_NEW_MINADBD := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+PLATFORM_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 10.0
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_LIBRESETPROP := true
---
+++ README.md
@@ -0,0 +1,51 @@
+# TWRP Samsung Galaxy A20
+![Galaxy A20](https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a20.jpg "Galaxy A20")
+# How-to install dependencies
+```
+# How-to clone source and device tree:
+
+mkdir -p ~/twrp && cd ~/twrp
+```
+$ repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0
+```
+# Clone a20 repo
+```
+$ git clone https://github.com/topser9/android_device_samsung_a20 -b twrp-9.0-new device/samsung/a20
+```
+# Sync
+
+$ repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j`nproc`
+```
+# How-to build:
+
+cd twrp; export ALLOW_MISSING_DEPENDENCIES=true; . build/envsetup.sh; lunch omni_a20dd-eng; mka recoveryimage
+```
+## How to find the image built
+```
+`cd /out/target/product/a20`
+```
+see recovery.img
+```
+# Device Tree for Samsung Galaxy A20 (SM-A205F)
+```
+Device Tree Made by topser99
+```
+# Specs
+|---------------------------------------------------------------------------------|
+|      Component        |          Specification                                  |
+|:----------------------|:--------------------------------------------------------|
+| Dimensions            | 158.4 x 74.7 x 7.8 mm (6.24 x 2.94 x 0.31 in)           |
+| Weight                | 169 g (5.96 oz)                                         |
+| Type                  | Super AMOLED                                            |
+| Size                  | 6.4 inches, 100.5 cm2 (~85.0% screen-to-body ratio)     |
+| Resolution            | 720 x 1560 pixels, 19.5:9 ratio (~268 ppi density)      |
+| OS                    |  Android 9.0 (Pie), One UI                              |
+| Chipset               | Exynos 7884 (14 nm)                                     |
+| CPU                   | Octa-core (2x1.6 GHz Cortex-A73 & 6x1.35 GHz Cortex-A53)|
+| GPU                   | Mali-G71 MP2                                            |
+| Internal              | 32GB 3GB RAM eMMC 5.1                                   |
+| USB                   | USB Type-C 2.0, USB On-The-Go                           |
+| Battery               | Li-Po 4000 mAh, non-removable                           |
+| charging              | Fast charging 15W                                       |
+| Status                | Available. Released 2019, April 05                      |
+|---------------------------------------------------------------------------------|
